b97589c0cbe8895a58f18226aa9956a721ef6be8
mukesh <mukesh.techy@gmail.com>
2018-01-27 23:24:20 +0530

    Initial Code

---
+++ src/main/java/com/demo/application/SampleApplication.java
@@ -0,0 +1,10 @@
+package com.demo.application;
+
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SampleApplication!!");
+	}
+
+}
