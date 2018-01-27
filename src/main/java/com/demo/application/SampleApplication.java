b4057c1fcf49a5d346d49e2ca704040d3a76cb20
mukesh <mukesh.techy@gmail.com>
2018-01-28 00:30:35 +0530

    Initial Architecture with out spring

--- src/main/java/com/demo/application/SampleApplication.java
+++ src/main/java/com/demo/application/SampleApplication.java
@@ -1,10 +1,18 @@
 package com.demo.application;
 
+import com.demo.service.EmployeeService;
+import com.demo.service.EmployeeServiceImpl;
+
 public class SampleApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sample Spring Application!!");
+		
+		EmployeeService employeeService = new EmployeeServiceImpl();
+		
+		
+		System.out.println(employeeService.getAllEmployee().get(0).getName());
 	}
 
 }
